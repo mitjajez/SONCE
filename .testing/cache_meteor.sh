65260d650e8b50612bb01353d3de2425cc88de22
Mitja Jež <mitja@xn--je-3va.si>
2016-08-18 20:24:24 +0200

    updated npm packages

--- .testing/cache_meteor.sh
+++ .testing/cache_meteor.sh
@@ -1,3 +1,2 @@
 # Cache Meteor
 if [ ! -e /home/ubuntu/.meteor/meteor ]; then curl https://install.meteor.com | sh; fi
-if [ -d /home/ubuntu/.meteor ]; then sudo ln -s /home/ubuntu/.meteor/meteor /usr/local/bin/meteor; fi
