a1e99008f79274166c506f0c46da7f11b70317ce
Mitja Jež <mitja@xn--je-3va.si>
2016-08-19 19:12:31 +0200

    diagnose meteor build with NODE_OPTIONS

--- .docker/build_app.sh
+++ .docker/build_app.sh
@@ -8,7 +8,7 @@ cd /source && npm install
 
 echo "Building app..."
 free -m
-NODE_OPTIONS=' --max_old_space_size=4096 ' meteor build --architecture=os.linux.x86_64 --directory /app --server=http://localhost:3000
+NODE_OPTIONS=' ulimit -n 999999 --max_old_space_size=4096 ' meteor build --architecture=os.linux.x86_64 --directory /app --server=http://localhost:3000
 rm -rf ~/.meteor
 rm /usr/local/bin/meteor
 rm -rf /source
