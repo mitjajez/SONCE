f44b1099a6114e721344d9f3c74e9dd0d82b41e7
Mitja Jež <mitja@xn--je-3va.si>
2016-08-19 18:40:47 +0200

    diagnose meteor build

--- .docker/build_app.sh
+++ .docker/build_app.sh
@@ -7,7 +7,8 @@ echo "Building app dependencies..."
 cd /source && npm install
 
 echo "Building app..."
-meteor build --architecture=os.linux.x86_64 --directory /app --server=http://localhost:3000
+free -m
+NODE_OPTIONS=' --max_old_space_size=4096 ' meteor build --architecture=os.linux.x86_64 --directory /app --server=http://localhost:3000
 rm -rf ~/.meteor
 rm /usr/local/bin/meteor
 rm -rf /source
